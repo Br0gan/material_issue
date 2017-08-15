68b5ecff08380ac75f70b08aac64f64375a07bbf
Corey <slicklogic@gmail.com>
2017-08-15 00:22:34 -0400

    first commit

---
+++ src/js/components/Layout.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default class Layout extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <nav className="navbar navbar">
+                    <div className="navbar-header">
+                        <h1>Suminoe Textiles <small>| Shop Order Material Issue</small></h1>
+                    </div>
+                    <ul className="nav navbar-nav navbar-right">
+                        <li><a href="#"><h3>Login</h3></a></li>
+                    </ul>
+                    <div className="col-md-12"><hr/></div>
+                </nav>
+            </div>
+        );
+    }
+}
