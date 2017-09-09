2920ddb0c9ab32f06e2e36b6f9aae52e4c4f1b36
Corey <slicklogic@gmail.com>
2017-09-09 19:36:37 -0400

    cleanned up issued

--- src/js/components/Issued.js
+++ src/js/components/Issued.js
@@ -4,61 +4,65 @@ import Loading from 'react-loading-overlay';
 
 export default class Issued extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {isActive: false}
+    super(props);
+    const { orderNo, userId, handleOrder, showAlert } = this.props;
+    this.state = {isActive: false};
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handQty = this.handleQty.bind(this);
   }
   handleQty(e) {
-    var id = e.target.id
-    this.setState({[id]: e.target.value})
+    var id = e.target.id;
+    this.setState({[id]: e.target.value});
   }
 
   handleSubmit(e) {
-    var id = e.target.id
-    if (!this.state[id] || this.state[id] == 0) {
+    var id = e.target.id;
+    var qtyUnissued = this.state[id];
+    if (!qtyUnissued || qtyUnissued == 0) {
       return
-    }
-    console.log(this.state[id])
-    this.setState({isActive: true})
+    };
+    this.setState({isActive: true});
     axios.post('/api/unissuemats', {
-      user_id: this.props.userId,
-      contract: 'STA',
-      order_no: this.props.orderNo,
+      user_id: userId,
+      order_no: orderNo,
       barcode_id: id,
-      qty_unissued: this.state[id]
+      qty_unissued: qtyUnissued
     })
     .then((res) => {
       if (res.data.status) {
         var e = {
             which: 13,
             target: {
-              value: this.props.orderNo,
+              value: orderNo,
             },
-          }
-          this.props.handleOrder(e)
-        this.props.showAlert(name + ': successfully unissed!', 'success', 3000)
+          };
+          handleOrder(e);
+        showAlert(name + ': successfully unissed!', 'success', 3000);
       } else {
-        this.props.showAlert(res.data.error, 'error', 0) 
+        showAlert(res.data.error, 'error', 0);
       }
-        document.getElementById(id).value = '';
-        this.setState({[id]: "", isActive: false})
+        document.getElementById(id).value = ''; 
+        this.setState({[id]: "", isActive: false});
     })
   }
 
   render() {
-    var issuedItems = this.props.issuedItems.map((part) =>
-          <tr key={part.lot_batch_no}>
+    function renderIssuedItems(part) {
+        return (
+        <tr key={part.lot_batch_no}>
             <td>{part.part_no}</td>
             <td>{part.barcode_id}</td>
             <td>{part.lot_batch_no}</td>
             <td>{part.qty_issued}</td>
             <td>
-                <input className="form-control un-qty" id={(part.barcode_id != 'No Barcode') ? part.barcode_id : part.lot_batch_no} type="number" onChange={this.handleQty.bind(this)}/>
+                <input className="form-control un-qty" id={(part.barcode_id != 'No Barcode') ? part.barcode_id : part.lot_batch_no} type="number" onChange={this.handleQty}/>
             </td>
             <td>
-                <button className="btn btn-sm btn-warning" id={(part.barcode_id != 'No Barcode') ? part.barcode_id : part.lot_batch_no} onClick={this.handleSubmit.bind(this)}>Submit</button>
+                <button className="btn btn-sm btn-warning" id={(part.barcode_id != 'No Barcode') ? part.barcode_id : part.lot_batch_no} onClick={this.handleSubmit}>Submit</button>
             </td>
-          </tr>
-    );
+        </tr>
+        )};
+
     return (
       <div>
         <div className="container" id="issued">
@@ -82,7 +86,7 @@ export default class Issued extends React.Component {
             </tr>
           </thead>
             <tbody>
-              {issuedItems}
+              {this.props.issuedItems.map(renderIssuedItems)}
             </tbody>
           </table>
           </Loading>
